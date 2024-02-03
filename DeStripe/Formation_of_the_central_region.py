954a79956b2bbbc96f1d4209514834b3e0baa899
anushka-220 <121439461+anushka-220@users.noreply.github.com>
2024-02-03 10:30:38 +0530

    added comments

--- DeStripe/Formation_of_the_central_region.py
+++ DeStripe/Formation_of_the_central_region.py
@@ -9,15 +9,15 @@ from Global_Sampling_of_Pixels import read_image, heterogeneity_func, global_pix
 
 def formation_central_region(Pn1):
     # Calculate the moment of inertia tensor
+    #here we have used the covariance matrix
     inertia_tensor = np.cov(Pn1, rowvar=False) #CHECK THIS LATER
 
-    # Calculate eigenvalues and eigenvectors of the tensor
+    #eigenvalues and eigenvectors of the tensor
     eigenvalues, eigenvectors = np.linalg.eigh(inertia_tensor)
 
     # Extract the major and minor axes lengths (σx, σy)
     sigma_x, sigma_y = np.sqrt(eigenvalues) # CHECK THIS LATER
 
-    # Calculate the initial radius
     initial_radius = np.sqrt(sigma_x + sigma_y)
 
     # Find the center of mass of Pn1
@@ -32,14 +32,14 @@ def formation_central_region(Pn1):
     while current_radius < initial_radius:
         current_radius += initial_radius / 10
 
-        # Create a circular mask centered at (center_i, center_j)
+        #circular mask centered at (center_i, center_j) i.e. the center of mass
         y, x = np.ogrid[:Pn1.shape[0], :Pn1.shape[1]]
         mask = ((x - center_j) ** 2 + (y - center_i) ** 2) <= current_radius ** 2
 
         # Label connected components in the mask
         labeled_mask, num_labels = label(mask)
 
-        # Find the label corresponding to the central region
+        # label corresponding to the central region
         central_label = labeled_mask[center_i, center_j]
 
         # Count Pn1 pixels and total pixels within the region
@@ -58,7 +58,7 @@ def formation_central_region(Pn1):
 
     return central_region, off_center_region
 
-image_path = r"D:\\Pranav\\Pictures\\Saved Pictures\\1_amplitude.jpg"
+image_path = r"C:\Users\ANUSHKA SINGH\Downloads\1_amplitude.jpg"
 image = read_image(image_path)
 
 # Assuming Pn1 is already obtained from the previous steps
